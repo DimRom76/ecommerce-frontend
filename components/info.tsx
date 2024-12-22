5f80a8e9a7b066bbab457d8fa253873196b57305
Dmytro Kotulskyi <102374866+DmytroKotulskyi@users.noreply.github.com>
2024-12-22 13:40:47 +0200

    added actions to Info component

--- components/info.tsx
+++ components/info.tsx
@@ -1,16 +1,26 @@
 "use client"
 
 import { ShoppingCart } from 'lucide-react';
+import { MouseEventHandler } from 'react';
 
 import { Product } from '@/types';
 import Currency from '@/components/ui/currency';
 import Button from '@/components/ui/button';
+import useCart from '@/hooks/use-cart';
 
 interface InfoProps {
   data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart();
+
+  const onAddToCard: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    cart.addItem(data);
+  };
+
   return (
     <div>
       <h1 className='text-3xl font-bold text-grey-900'>{data.name}</h1>
@@ -20,22 +30,23 @@ const Info: React.FC<InfoProps> = ({ data }) => {
       <hr className='my-4' />
       <div className='flex flex-col gap-y-6'>
         <div className='flex items-center gap-x-4'>
-            <h3 className='font-semibold text-black'>Size:</h3>
-            <div>
+          <h3 className='font-semibold text-black'>Size:</h3>
+          <div>
             {data?.size?.name} / {data?.size?.value}
-            </div>
+          </div>
         </div>
         <div className='flex items-center gap-x-4'>
-            <h3 className='font-semibold text-black'>Color:</h3>
-            <div 
-                className='h-6 w-6 rounded-full border border-gray-600' 
-                style={{ backgroundColor: data?.color?.value}}/>
+          <h3 className='font-semibold text-black'>Color:</h3>
+          <div
+            className='h-6 w-6 rounded-full border border-gray-600'
+            style={{ backgroundColor: data?.color?.value }}
+          />
         </div>
       </div>
       <div className='mt-10 flex items-center gap-x-3'>
-        <Button className='flex items-center gap-x-2'>
-            Add to card
-            <ShoppingCart />
+        <Button onClick={onAddToCard} className='flex items-center gap-x-2'>
+          Add to card
+          <ShoppingCart />
         </Button>
       </div>
     </div>
